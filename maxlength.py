f29445971ec56f5df75142a1413caf790093ce7b
Arne Lehmann <arne.lehmann@gmx.de>
2017-09-27 20:33:54 +0200

    more 'pythonic' way, i think

--- maxlength.py
+++ maxlength.py
@@ -5,12 +5,7 @@ If two numbers in the argument array have the same number of digits, return the 
 
 https://www.codewars.com/kata/58daa7617332e59593000006/train/python
 '''
-
 def find_longest(arr):
-    longest_number = min(arr) - 1
-    for x in arr:
-        xstr = str(x)
-        if len(xstr) > len(str(longest_number)):
-            longest_number = x
+    return max(arr, key=lambda x: len(str(x)))    
 
-    return longest_number
+print(find_longest([123, 345, 56666]))
