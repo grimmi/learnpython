ed2d85fae76fcc366e9f0fe98df73931fda9fa10
Arne Lehmann <arne.lehmann@gmx.de>
2017-09-27 20:36:12 +0200

    made the linter happier

--- maxlength.py
+++ maxlength.py
@@ -1,11 +1,12 @@
 '''
 Find the number with the most digits.
 
-If two numbers in the argument array have the same number of digits, return the first one in the array.
+If two numbers in the argument array have the same number of digits,
+return the first one in the array.
 
 https://www.codewars.com/kata/58daa7617332e59593000006/train/python
 '''
 def find_longest(arr):
-    return max(arr, key=lambda x: len(str(x)))    
+    return max(arr, key=lambda x: len(str(x)))
 
 print(find_longest([123, 345, 56666]))
