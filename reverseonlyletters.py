088ea672ca5e760f0bc7c11dc3cc01b03eb0d140
Arne Lehmann <arne.lehmann@gmx.de>
2017-09-27 20:39:58 +0200

    starting with string reversal (again... ;) )

---
+++ reverseonlyletters.py
@@ -0,0 +1,23 @@
+'''
+Task
+
+Given a string str, reverse it omitting all non-alphabetic characters.
+Example
+
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+Input/Output
+
+    [input] string str
+
+    A string consists of lowercase latin letters, digits and symbols.
+
+    [output] a string
+
+'''
+
+def reverse_letter(string):
+    return ''.join([x for x in reversed(string) if x.isalpha()])
+
+print(reverse_letter("hallo"))
