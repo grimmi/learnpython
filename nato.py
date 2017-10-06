d96aa4525e73589eef683f29f7cbd2300435c598
Arne Lehmann <arne.lehmann@gmx.de>
2017-10-06 19:29:07 +0200

    replaced != ' ' with .isspace

--- nato.py
+++ nato.py
@@ -29,6 +29,6 @@ NATO = {"A": "Alfa", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo", "F
         "Y": "Yankee", "Z": "Zulu"}
 
 def to_nato(words):
-    return ' '.join([NATO.get(c.upper(), c) for c in words if c != ' '])
+    return ' '.join([NATO.get(c.upper(), c) for c in words if not c.isspace()])
 
 print(to_nato("if you can read"))
