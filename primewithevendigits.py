2525bd792b6689ba5b707040e404846ffc0c6dd4
Arne Lehmann <arne.lehmann@gmx.de>
2017-12-19 11:30:15 +0100

    naive implementation working for the basic test cases

--- primewithevendigits.py
+++ primewithevendigits.py
@@ -18,7 +18,33 @@ f(487) ---> 467
 taken from https://www.codewars.com/kata/582dcda401f9ccb4f0000025/train/python
 '''
 
+from math import sqrt
+
 def count_even_digits(n):
     return sum((str(n).count(x) for x in ['0', '2', '4', '6', '8']))
 
-print(count_even_digits(24))
+def is_prime(n):
+    if n in [2, 3, 5, 7, 11, 13, 17, 19]:
+        return True
+    if n < 23:
+        return False
+    upper_limit = int(sqrt(n))
+
+    for candidate in range(2, upper_limit):
+        if n % candidate == 0:
+            return False
+
+    return True
+
+def f(n):
+    max_prime = 2
+    max_even_count = 1
+    for x in range(23, n):
+        if is_prime(x):
+            digitcount = count_even_digits(x)
+            if x > max_prime and digitcount >= max_even_count:
+                max_even_count = digitcount
+                max_prime = x
+    return max_prime
+
+print(f(10000))
